ab39a4cfe0539f39cb28fad498e0f1c803e98c8d
unknown <example@example.com>
2013-11-21 08:24:03 -0600

    Consolidated all migrations into a single migration

---
+++ db/migrate/001_create_vtrack.rb
@@ -0,0 +1,51 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.datetime :first_at
+      t.datetime :last_at
+      t.integer :wday
+      t.integer :s_hr
+      t.integer :s_min
+      t.integer :e_hr
+      t.integer :e_min
+
+      t.timestamps
+    end
+
+    create_table :statuses do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  
+    create_table :work_statuses do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  
+    create_table :workers do |t|
+      t.string :name
+      t.string :image
+      t.boolean :in_shop
+      t.string :email
+      t.string :phone
+      t.references :status, index: true
+      t.references :work_status, index: true
+      t.boolean :public_email
+
+      t.timestamps
+    end
+  
+    create_table :work_times do |t|
+      t.datetime :start_at
+      t.datetime :end_at
+      t.references :worker, index: true
+      t.references :status, index: true
+      t.references :work_status, index: true
+
+      t.timestamps
+    end
+  end
+end
--- db/migrate/20131121141755_create_work_statuses.rb
+++
@@ -1,9 +0,0 @@
-class CreateWorkStatuses < ActiveRecord::Migration
-  def change
-    create_table :work_statuses do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end

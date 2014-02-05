acf4c03dc663bcba516e8865efeafbad67e6ae1c
unknown <example@example.com>
2014-02-04 23:40:49 -0600

    Add link_to_survey helper to LinktoHelper

--- app/helpers/linkto_helper.rb
+++ app/helpers/linkto_helper.rb
@@ -2,4 +2,13 @@ module LinktoHelper
   def link_to_worker(worker, options = {})
     link_to(worker.name, worker) if worker
   end
+
+  def link_to_survey(worker, options = {})
+    #link_to(worker.name, worker) if worker
+    if worker && worker.survey
+      link_to("Survey", edit_survey_path(worker.survey))
+    else
+      link_to("Take Survey", new_survey_path(worker_id: worker.id))
+    end
+  end
 end

00f3a85fdc4005851dfe94c4b7191af0d19031ac
unknown <example@example.com>
2014-02-08 20:30:59 -0600

    Updated surveys#form, #boolean_row partial with suggestions from Recyclery

--- app/helpers/linkto_helper.rb
+++ app/helpers/linkto_helper.rb
@@ -6,9 +6,9 @@ module LinktoHelper
   def link_to_survey(worker, options = {})
     #link_to(worker.name, worker) if worker
     if worker && worker.survey
-      link_to("Survey", edit_survey_path(worker.survey))
+      link_to("Profile", edit_survey_path(worker.survey))
     else
-      link_to("Take Survey", new_survey_path(worker_id: worker.id))
+      link_to("Create Profile", new_survey_path(worker_id: worker.id))
     end
   end
 end

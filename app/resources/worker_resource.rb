b631e64d4434082f8f0e815ef2d530d32ddd1466
unknown <example@example.com>
2014-08-22 03:54:11 -0500

    Add shop, active, and missing class methods to WorkerResource Add sign_in and sign_out instance methods to WorkerResource

--- app/resources/worker_resource.rb
+++ app/resources/worker_resource.rb
@@ -1,2 +1,25 @@
 class WorkerResource < VtrackApi
+  class << self
+    def shop
+      find(:all, from: :shop)
+    end
+
+    def active
+      find(:all, from: :active)
+    end
+
+    def missing
+      find(:all, from: :missing)
+    end
+
+  end
+
+  def sign_in
+    post(:sign_in)
+  end
+
+  def sign_out
+    post(:sign_out)
+  end
+
 end # class WorkerResource < VtrackApi

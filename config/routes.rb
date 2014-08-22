72b35116ff83a0e86cf5b72f39efbc1b3402b034
unknown <example@example.com>
2014-08-22 03:13:01 -0500

    Add config/routes/api_routes.rb and split up config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,7 @@
-require 'api_constraints'
+Dir.glob("#{Rails.root.to_s}/config/routes/**/*.rb").each {|route_file| load(route_file)}
 
 Rails.application.routes.draw do
-
-  concern :api_v1 do
-    scope :backup, controller: 'backup' do
-      get :surveys
-      get :events
-      get :workers
-      get :work_times
-      get :work_statuses
-      get :statuses
-    end
-
-    resources :workers do
-      collection do
-        get :shop
-        get :active
-        get :missing
-      end
-      member do
-        post :sign_in
-        post :sign_out
-      end
-    end
-
-    resources :work_times do
-      collection do
-        get :logged_in
-        get :too_long
-        get :mismatched_dates
-
-        get :week
-        get :month
-        get :year
-      end
-    end
-
-    resources :surveys
-    resources :events
-
-  end
-
-  namespace :api, defaults: {format: 'json'} do
-    scope( :module => :v1,
-           constraints: ApiConstraints.new(version: 1, default: true) ) do
-      concerns :api_v1
-    end
-    namespace :v1 do
-      concerns :api_v1
-    end
-  end
+  Routes::ApiRoutes.draw(self)
 
   root 'shop#index'
 
---
+++ config/routes/api_routes.rb
@@ -0,0 +1,60 @@
+require 'api_constraints'
+module Routes
+  module ApiRoutes
+
+    def self.draw(context)
+      context.instance_eval do
+
+        concern :api_v1 do
+          scope :backup, controller: 'backup' do
+            get :surveys
+            get :events
+            get :workers
+            get :work_times
+            get :work_statuses
+            get :statuses
+          end
+
+          resources :workers do
+            collection do
+              get :shop
+              get :active
+              get :missing
+            end
+            member do
+              post :sign_in
+              post :sign_out
+            end
+          end
+
+          resources :work_times do
+            collection do
+              get :logged_in
+              get :too_long
+              get :mismatched_dates
+
+              get :week
+              get :month
+              get :year
+            end
+          end
+
+          resources :surveys
+          resources :events
+
+        end
+
+        namespace :api, defaults: {format: 'json'} do
+          scope(:module => :v1,
+                constraints: ApiConstraints.new(version: 1, default: true)) do
+            concerns :api_v1
+          end
+          namespace :v1 do
+            concerns :api_v1
+          end
+        end
+
+      end
+    end
+  end
+end

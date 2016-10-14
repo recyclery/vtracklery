925bbb39f91c7530c2f05eba738c8fde048a9762
unknown <example@example.com>
2016-10-14 14:42:02 -0500

    Update ApiBasic (in scripts) to use clock_in instead of sign_in, etc. Rename ApiBasic#api_get_worker to api_get_worker_by_email Create ApiBasic#api_get_worker_by_phone Create ApiBasic#get_email and #get_phone Add parameter checking to ApiBasic#get_email, #get_phone, #clock_in, and   #clock_out Update USAGE message in top comments

--- scripts/api_basic.rb
+++ scripts/api_basic.rb
@@ -7,9 +7,12 @@ require 'json'
 #    #!/usr/bin/env ruby
 #    require_relative 'api_basic'
 #    email = ARGV[0] || "example@example.com"
+#    phone = ARGV[1] || "18475551212"
 #    vtrack = ApiBasic.new('http://vtrack.domain')
-#    worker_id = vtrack.api_get_worker(email)
-#    vtrack.sign_in(worker_id)
+#    worker_id = vtrack.api_get_worker_by_email(email)
+#    vtrack.clock_in(worker_id)
+#    worker_id = vtrack.api_get_worker_by_phone(phone)
+#    vtrack.clock_out(worker_id)
 #
 class ApiBasic
   URL = 'http://localhost:3000'
@@ -32,8 +35,21 @@ class ApiBasic
 
   # @param email [String] the email for the worker
   # @return [Integer] the worker's database id
-  def api_get_worker(email)
-    response = @conn.get 'workers/email', { value: email }
+  def api_get_worker_by_email(email)
+    response = @conn.get 'workers/where', { email: email }
+    workers = response.body
+
+    if 1 == workers.count
+      worker = workers.first
+      worker_id = worker['id']
+      return worker_id
+    end
+  end
+
+  # @param phone [String] the phone for the worker
+  # @return [Integer] the worker's database id
+  def api_get_worker_by_phone(phone)
+    response = @conn.get 'workers/where', { phone: phone }
     workers = response.body
 
     if 1 == workers.count
@@ -46,9 +62,30 @@ class ApiBasic
   # @param worker_id [Integer] the worker's database id
   # @param e [Integer] the unix time (epoch) the worker clocked in
   # @return [Hash]
-  def sign_in(worker_id, e = nil)
-    if e.nil? then response = @conn.post "workers/#{worker_id}/sign_in"
-    else response = @conn.post("workers/#{worker_id}/sign_in", { epoch: e })
+  def get_email(worker_id)
+    raise "Invalid worker_id: #{worker_id}" if ["",nil].include?(worker_id)
+    response = @conn.get "workers/#{worker_id}/email"
+
+    return response.body
+  end
+
+  # @param worker_id [Integer] the worker's database id
+  # @param e [Integer] the unix time (epoch) the worker clocked in
+  # @return [Hash]
+  def get_phone(worker_id)
+    raise "Invalid worker_id: #{worker_id}" if ["",nil].include?(worker_id)
+    response = @conn.get "workers/#{worker_id}/phone"
+
+    return response.body
+  end
+
+  # @param worker_id [Integer] the worker's database id
+  # @param e [Integer] the unix time (epoch) the worker clocked in
+  # @return [Hash]
+  def clock_in(worker_id, e = nil)
+    raise "Invalid worker_id: #{worker_id}" if ["",nil].include?(worker_id)
+    if e.nil? then response = @conn.post "workers/#{worker_id}/clock_in"
+    else response = @conn.post("workers/#{worker_id}/clock_in", { epoch: e })
     end
 
     return response.body
@@ -57,9 +94,10 @@ class ApiBasic
   # @param worker_id [Integer] the worker's database id
   # @param epoch [Integer] the unix time (epoch) the worker clocked out
   # @return [Hash]
-  def sign_out(worker_id, e = nil)
-    if e.nil? then response = @conn.post "workers/#{worker_id}/sign_out"
-    else response = @conn.post("workers/#{worker_id}/sign_out", { epoch: e })
+  def clock_out(worker_id, e = nil)
+    raise "Invalid worker_id: #{worker_id}" if ["",nil].include?(worker_id)
+    if e.nil? then response = @conn.post "workers/#{worker_id}/clock_out"
+    else response = @conn.post("workers/#{worker_id}/clock_out", { epoch: e })
     end
 
     return response.body

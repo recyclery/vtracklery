236b04405fbfb43c18aa5f2ded93ccc65360b3b4
2B8BD7CE <example@example.com>
2019-01-09 17:54:08 -0600

    Add Status class methods :member, :staff, :volunteer, and :youth

--- app/models/status.rb
+++ app/models/status.rb
@@ -14,6 +14,26 @@ class Status < ActiveRecord::Base
   MEMBER = "Member"
   STAFF =  "Paid Staff"
 
+  # @return [Boolean] true if Worker has "Member" status
+  def self.member
+    return where(name: Status::MEMBER).first
+  end
+
+  # @return [Boolean] true if Worker has "Staff" status
+  def self.staff
+    return where(name: Status::STAFF).first
+  end
+
+  # @return [Boolean] true if Worker has "Volunteer" status
+  def self.volunteer
+    return where(name: Status::VOLUNTEER).first
+  end
+
+  # @return [Boolean] true if Worker has "Youth" status
+  def self.youth
+    return where(name: Status::YOUTH).first
+  end
+
   # Attributes accessible via the API
   API_ATTRIBUTES = [ :name, :created_at, :updated_at ]
 

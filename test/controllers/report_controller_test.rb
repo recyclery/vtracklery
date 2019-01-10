4e822ab8f2ad8f92611d70b9af6d3c1420bb3504
2B8BD7CE <example@example.com>
2019-01-09 20:04:59 -0600

    Add tests for new reports and alternate date routes to reports

--- test/controllers/report_controller_test.rb
+++ test/controllers/report_controller_test.rb
@@ -31,8 +31,23 @@ class ReportControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get month with year" do
+    get :month, year: 2018
+    assert_response :success
+  end
+
+  test "should get month with year and month" do
+    get :month, year: 2018, month: 6
+    assert_response :success
+  end
+
   test "should get monthly" do
     get :monthly
+    assert_response :success
+  end
+
+  test "should get monthly with year" do
+    get :monthly, year: 2018
     assert_response :success
   end
 
@@ -52,13 +67,63 @@ class ReportControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get week with year" do
+    get :week, year: 2018
+    assert_response :success
+  end
+
+  test "should get week with year and month" do
+    get :week, year: 2018, month: 6
+    assert_response :success
+  end
+
+  test "should get week with year, month, and day" do
+    get :week, year: 2018, month: 6, day: 15
+    assert_response :success
+  end
+
   test "should get weekly" do
     get :weekly
     assert_response :success
   end
 
+  test "should get weekly with year" do
+    get :weekly, year: 2018
+    assert_response :success
+  end
+
+  test "should get weekly with year and month" do
+    get :weekly, year: 2018, month: 6
+    assert_response :success
+  end
+
   test "should get year" do
     get :year
+    assert_response :success
+  end
+
+  test "should get year with year" do
+    get :year, year: 2018
+    assert_response :success
+  end
+
+  test "should get member_hours_year_report with year" do
+    get :year_hoursm, year: 2018
+    assert_response :success
+  end
+
+  test "should get staff_hours_year_report with year" do
+    get :year_hourss, year: 2018
+    assert_response :success
+  end
+
+  test "should get volunteer_hours_year_report with year" do
+    get :year_hoursv, year: 2018
+    assert_response :success
+  end
+
+  test "should get youth_hours_year_report with year" do
+    get :year_hoursy, year: 2018
     assert_response :success
   end
 
